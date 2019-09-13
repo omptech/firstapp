50d957d797e47c39be8b9db2941e3df7661cfdee
omptech <omprakashlodhi38@gmail.com>
2019-09-13 21:27:42 +0530

    Finally resolved the error of firebase

--- app/src/main/java/com/blogspot/okyser/learnearn/MainActivity.java
+++ app/src/main/java/com/blogspot/okyser/learnearn/MainActivity.java
@@ -2,13 +2,6 @@ package com.blogspot.okyser.learnearn;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +9,15 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
--- app/src/main/java/com/blogspot/okyser/learnearn/RegisterActivity.java
+++ app/src/main/java/com/blogspot/okyser/learnearn/RegisterActivity.java
@@ -2,23 +2,66 @@ package com.blogspot.okyser.learnearn;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
     EditText uname,upassword,usemail;
     Button regbtn;
     TextView Alrlgn;
+    String add;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        uname=findViewById(R.id.username);
+        usemail=findViewById(R.id.useremail);
+        upassword=findViewById(R.id.userpassword);
+        regbtn=findViewById(R.id.btnregister);
+        Alrlgn=findViewById(R.id.alreadylogin);
+        firebaseAuth=FirebaseAuth.getInstance();
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                if(validate())
+                {
+
+
+                    String user_email=usemail.getText().toString().trim();
+                    String user_pass=upassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email,user_pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                            }
+                            else
+                            {
+                                Toast.makeText(RegisterActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
         Alrlgn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
